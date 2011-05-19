eb37edf3332e03f6de22e8fa723b90a799e87fbc
Василий Большаков <va.bolshakov@gmail.com>
2011-05-19 21:59:34 +0400

    uwsgi.py changed

--- uwsgi.py
+++ uwsgi.py
@@ -3,6 +3,7 @@ import os, sys
 from django.core.handlers.wsgi import WSGIHandler
 
 
+sys.path.insert(0, '/usr/local/www/akar')
 sys.stdout = sys.stderr
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 application = WSGIHandler()
