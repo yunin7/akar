23fb5976d56187548b5f4e8b02363ac498627dcb
Василий Большаков <va.bolshakov@gmail.com>
2011-08-09 04:01:48 +0400

    remove unused uwsgi.py

--- uwsgi.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-
-from django.core.handlers.wsgi import WSGIHandler
-
-
-sys.path.insert(0, '/usr/local/www/akar')
-sys.stdout = sys.stderr
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-application = WSGIHandler()
